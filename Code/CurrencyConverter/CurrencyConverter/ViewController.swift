dd1ab933e8eb8da24a3de6ed0ecc62c2447c5709
Nicolás Miari <nicolasmiari@gmail.com>
2019-10-04 14:12:59 +0900

    Add Empty Project

---
+++ Code/CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Nicolás Miari on 2019/10/04.
+//  Copyright © 2019 Nicolás Miari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
